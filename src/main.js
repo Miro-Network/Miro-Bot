9942a61e8a1d3d9103024902563cdbd63d4e4393
Nhu Phuc <phucho0237@gmail.com>
2022-12-31 16:45:10 +0700

    nextjs

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
-const express = require("express");
-const config = require("./config");
+// const express = require("express");
+// const path = require("path");
+
+// const config = require("./config");
 
 const { loadClient } = require("./miro");
 
-let term = require("terminal-kit").terminal;
+// let term = require("terminal-kit").terminal;
 
-const app = express();
-const http = require("http").Server(app);
+// const app = express();
+// const http = require("http").Server(app);
 
-app.use("/", (req, res) => res.send("404"));
+// app.use("/", (req, res) => {
+//    res.sendFile(path.join(__dirname, "./website/base/index.html"));
+// });
 
-// Require bot file
-loadClient().then(() => {
-   http.listen(config.port);
-   term.cyan(`> [WEB] | Listen on http://localhost:${config.port}\n`);
-});
+// // Require bot file
+// loadClient().then(() => {
+//    http.listen(config.port);
+//    term.cyan(`> [WEB] | Listen on http://localhost:${config.port}\n`);
+// });
+
+loadClient();
