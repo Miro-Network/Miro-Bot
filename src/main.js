cec1847193b7668544ac6c560e8be7c76796896a
Nhu Phuc <phucho0237@gmail.com>
2022-12-31 16:09:05 +0700

    prepare for website

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const config = require("./config");
 
+const { loadClient } = require("./miro");
+
+let term = require("terminal-kit").terminal;
+
 const app = express();
 const http = require("http").Server(app);
 
-// Require bot file
-require("./miro");
-
 app.use("/", (req, res) => res.send("404"));
 
-http.listen(config.port);
+// Require bot file
+loadClient().then(() => {
+   http.listen(config.port);
+   term.cyan(`> [WEB] | Listen on http://localhost:${config.port}\n`);
+});
--- src/miro.js
+++ src/miro.js
@@ -1,28 +1,27 @@
-const {
-   Client,
-   GatewayIntentBits,
-   Partials,
-   Collection,
-} = require("discord.js");
-const express = require("express");
+function loadClient() {
+   const {
+      Client,
+      GatewayIntentBits,
+      Partials,
+      Collection,
+   } = require("discord.js");
 
-const { loadEvents } = require("./handlers/events");
-const { loadCommands } = require("./handlers/commands");
+   const { loadEvents } = require("./handlers/events");
+   const { loadCommands } = require("./handlers/commands");
 
-let term = require("terminal-kit").terminal;
+   const client = new Client({
+      allowedMentions: { parse: ["users", "roles"], repliedUser: true },
+      intents: [Object.keys(GatewayIntentBits)],
+      partials: [Object.keys(Partials)],
+   });
 
-const client = new Client({
-   allowedMentions: { parse: ["users", "roles"], repliedUser: true },
-   intents: [Object.keys(GatewayIntentBits)],
-   partials: [Object.keys(Partials)],
-});
+   client.commands = new Collection();
+   client.config = require("./config");
 
-client.commands = new Collection();
-client.config = require("./config");
+   return client.login(client.config.token).then(() => {
+      loadEvents(client);
+      loadCommands(client);
+   });
+}
 
-client.login(client.config.token).then(() => {
-   loadEvents(client);
-   loadCommands(client);
-});
-
-module.exports = client;
+module.exports = { loadClient };
