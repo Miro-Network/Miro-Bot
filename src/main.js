ec17f17bba1bc253f82277eda3500708aae49e31
Nhu Phuc <phucho0237@gmail.com>
2023-01-08 13:04:15 +0700

    some fix

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ function loadWeb() {
 
    app.get("/", (req, res) => res.send("Online!"));
    app.listen(config.web.port, () =>
-      term.cyan(`> [WEB] | Listening on http://localhost:${config.web.port}\n`)
+      term.cyan(`> [WEB] | Listening on port ${config.web.port}\n`)
    );
 }
 
